2a959624c736425833b636739a9d35ac44a2998e
Michael Favour <admin@example.com>
2026-03-19 14:32:42 +0100

    Fix favicon rendering with root route and manifest link

--- amfit/urls.py
+++ amfit/urls.py
@@ -5,8 +5,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path(
+        'favicon.ico',
+        RedirectView.as_view(url=f"{settings.STATIC_URL}images/favicon.ico", permanent=True),
+    ),
     path(settings.ADMIN_URL, admin.site.urls),
     path('account/', include('users.urls')),
     path('', include('orders.urls')),
